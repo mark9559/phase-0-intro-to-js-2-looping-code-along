b578e41db11e75fe916c51390c7531d7ddc78b2e
Mark Mwangi <mwangimark57@gmail.com>
2023-09-17 14:48:57 +0300

    COmpleted Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Example usage:
+  const names = ["Ada", "Brendan", "Ali"];
+  const eventName = "Birthday Party";
+  const thankYouMessages = writeCards(names, eventName);
+  console.log(thankYouMessages);
+  
+
+  countDown(5);
+  
